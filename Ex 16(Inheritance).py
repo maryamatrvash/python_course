8776d510213142fe82cd341945569cff54f6a9b4
MaryamAtrvash <maryamatrvash83@gmail.com>
2024-10-08 16:44:01 +0330

    solve Question 1

---
+++ Ex 16(Inheritance).py
@@ -0,0 +1,47 @@
+class Device:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def turn_on(self):
+        print("Device is powered on!")
+
+    def turn_off(self):
+        print("Device is powered off!")
+
+    def __str__(self):
+        return f"Brand: {self.brand}, Model: {self.model}, Price: {self.price}" 
+    
+class Laptop(Device):
+    def __init__(self, brand, model, price, ram):
+        super().__init__(brand, model, price)
+        self.ram_size = ram
+
+    def open_laptop(self):
+        print("Laptop is opened!") 
+
+    def __str__(self):
+        info = super().__str__()
+        return f"{info}, Ram Size: {self.ram_size} GB" 
+    
+class Smartphone(Device):
+    def __init__(self, brand, model, price, camera):
+        super().__init__(brand, model, price)
+        self.camera_resolution = camera
+
+    def take_photo(self):
+        print("A photo has been taken!")
+
+    def __str__(self):
+        info = super().__str__()
+        return f"{info}, Camera Resolution: {self.camera_resolution} MP" 
+    
+
+l1 = Laptop("a", "12", 200, 4)
+l1.open_laptop() 
+print(l1) 
+s1 = Smartphone("b", "20", 1000, 45)
+s1.take_photo() 
+s1.turn_off() 
+print(s1) 
