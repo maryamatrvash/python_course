0366a69174362dbbda26870676bcc58dc59d0d8c
MaryamAtrvash <maryamatrvash83@gmail.com>
2024-11-06 13:41:26 +0330

    Add files and product file

---
+++ Ex 22 (DigitalStore)/product.py
@@ -0,0 +1,14 @@
+class Product:
+    def __init__(self, id_: str, name: str, price: float, brand: str):  
+        self.id_ = id_
+        self.name = name
+        self.price = price
+        self.brand = brand 
+
+    def display(self):
+        print(f"ID: {self.id_}, Name: {self.name}, Price: {self.price}, Brand: {self.brand}") 
+
+
+if __name__ == "__main__":
+    p = Product("123", "p1", 22.5, "b")
+    p.display() 
