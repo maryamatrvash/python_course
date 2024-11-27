5d13e846c19a77fc0af718b0540534a4c0075494
MaryamAtrvash <maryamatrvash83@gmail.com>
2024-11-27 14:19:58 +0330

    compelete q1

--- Ex_23.py
+++ Ex_23.py
@@ -1,4 +1,4 @@
-import requests  # type: ignore
+import requests 
 class Music:
     def __init__(self, name, singer, album, link):
         self.name = name
@@ -13,12 +13,30 @@ class SongFetcher:
     def __init__(self, url):
         self.url = url 
 
-    def search_track(self):
+    @staticmethod 
+    def read_data(song):
+        name = song["trackName"] 
+        artist = song["artistName"] 
+        album = song.get("collectionName", "No Album")
+        link = song.get("trackViewUrl", "No Link")
+        return name, artist, album, link 
+
+    def search_track(self, name): 
         respond = requests.get(self.url) 
         data = respond.json() 
-        print(data) 
+        track_data = data["results"] 
+        tracks = [] 
+        for song in track_data:
+            if name == song["trackName"]:
+                name, artist, album, link = self.read_data(song) 
+                music_obj = Music(name, artist, album, link) 
+                tracks.append(music_obj)
+                return tracks 
+            else:
+                print(f"{name} Not Found!") 
 
 if __name__ == "__main__":
-    url = "https://itunes.apple.com/search?term={track_name}&media=music" 
+    track_name = input("Enter track name for search: ") 
+    url = f"https://itunes.apple.com/search?term={track_name}&media=music" 
     obj = SongFetcher(url) 
-    obj.search_track() 
+    obj.search_track(track_name)  
